1b56aabf2363c6b467e2b4abb789ab77e7ab16be
Kao_TzuChiao <>
2023-05-24 16:59:01 +0900

    C++用のプロジェクトを作成しました

---
+++ Source/VrProject/VrProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VrProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRPROJECT_API AVrProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
